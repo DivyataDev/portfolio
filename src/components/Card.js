104de08f504ed4e6d2beb6f51b4c790bc5f89c79
Divyata <divyata.phadte@gmail.com>
2024-01-06 17:55:56 +0530

    Update Card.js
    Changes to the Card Component in terms of presentation.

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,21 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  // Implemented the UI for the Card component according to the instructions.
+  // Implemented the component with the elements imported above. 
+  return (
+    
+      <VStack color="black" backgroundColor="white" borderRadius="10" >
+        <Image src={imageSrc} alt={title} borderRadius="10" />
+        <VStack  p={4} alignItems="flex-start" spacing={4}>
+          <Heading size="md">{title}</Heading>
+          <Text color="#808080">{description}</Text>
+          <a href="#">See More&nbsp;&nbsp;<FontAwesomeIcon icon={faArrowRight} size="1x" /></a>
+        </VStack>
+      </VStack>
+ 
+
+  );
 };
 
 export default Card;
