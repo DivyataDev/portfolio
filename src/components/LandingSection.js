57dc57121b7fbce024c3935335f2e585ebbd0aca
Divyata <divyata.phadte@gmail.com>
2024-01-06 15:35:21 +0530

    Update LandingSection.js
    css changes

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -14,14 +14,14 @@ const LandingSection = () => (
     alignItems="center"
     isDarkBackground
     backgroundColor="#2A4365"
-  >
+  > 
      <VStack>
         <Avatar size="2xl" src="https://i.pravatar.cc/150?img=7 "></Avatar>
-        <Heading size="md">{greeting}</Heading><br/><br/>            
-        <Heading size="2xl">{bio1}</Heading>
+        <Heading size="md">{greeting}</Heading>       
+        <Heading size="2xl" mt="42">{bio1}</Heading>
         <Heading size="2xl">{bio2}</Heading>
       </VStack>
-     
+      
    
 
   </FullScreenSection>
